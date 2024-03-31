221b949ffc68b69694b0d106e422859a33217abf
Lisowez <lisowez@icloud.com>
2024-03-31 16:40:25 +0300

    feat: feat fifth ticket

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,8 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funct) {
+  return funct.map((func) => func.length);
 }
 
 /**
